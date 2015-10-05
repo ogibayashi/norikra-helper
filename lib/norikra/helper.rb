f4041fe7427e304443d3aa371abee2bc59d79404
Hironori Ogibayashi <hironori.ogibayashi@linecorp.com>
2015-10-05 20:13:38 +0900

    Add query test feature.

--- lib/norikra/helper.rb
+++ lib/norikra/helper.rb
@@ -47,6 +47,49 @@ module Norikra
     end
   end
 
+  class Query < Thor
+    include Norikra::Client::CLIUtil
+    desc "test QUERY", "test QUERY. Register temporary query"
+    option :format, :type => :string, :default => 'json', :desc => "format of output data per line of stdout [json(default), ltsv]"
+    option :time_key, :type => :string, :default => 'time', :desc => "output key name for event time (default: time)"
+    option :time_format, :type => :string, :default => '%Y/%m/%d %H:%M:%S', :desc => "output time format (default: '2013/05/14 17:57:59')"
+    option :query_file, :type => :string, :default => nil, :desc => "read query from file", :aliases => :f
+    
+    def test(query=nil)
+      @query_name_prefix = "norikra-tmp-"
+      @query_group = "norikra-helper"
+
+      if query && options[:query_file]
+        puts "Both query string and --query_file were specified"
+        exit 1
+      elsif not (query || options[:query_file])
+        puts "Please specify query string or file."
+        exit 1
+      elsif options[:query_file]
+        query = File.open(options[:query_file]).read
+      end
+
+      
+      formatter = formatter(options[:format])
+      time_formatter = lambda{|t| Time.at(t).strftime(options[:time_format])}
+
+      query_name = @query_name_prefix + "#{$$}_#{Time.now.to_i}"
+      client(parent_options).register(query_name, @query_group, query)
+      puts "Registered query: #{query_name}"
+      begin
+        while true
+          client(parent_options).event(query_name).each do |time,event|
+            event = {options[:time_key] => Time.at(time).strftime(options[:time_format])}.merge(event)
+            puts formatter.format(event)
+          end
+          sleep 1
+        end
+      ensure
+        client(parent_options).deregister(query_name)
+      end
+    end
+  end
+  
   class HelperCLI < Thor
     include Norikra::Client::CLIUtil
 
@@ -56,5 +99,7 @@ module Norikra
     
     desc "target CMD ...ARGS", "use targets"
     subcommand "target", Target
+    desc "query CMD ...ARGS", "use query"
+    subcommand "query", Query
   end
 end

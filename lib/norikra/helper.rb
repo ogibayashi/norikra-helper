1fc5e52ac069546b9a481a8a8518f46b45304350
Hironori Ogibayashi <hironori.ogibayashi@linecorp.com>
2015-11-24 18:15:04 +0900

    Reuse norikra-client classes when possible.

--- lib/norikra/helper.rb
+++ lib/norikra/helper.rb
@@ -1,79 +1,18 @@
-require "norikra/helper/version"
 require "thor"
-require "norikra/client"
 require "norikra/client/cli"
-require "net/http"
-require "json"
-require "timeout"
+require "norikra/client"
 
 module Norikra::Helper
   QUERY_NAME_PREFIX = "norikra-tmp-"
   QUERY_GROUP = "norikra-helper"
 
-  class Target < Norikra::Client::Target
+  class Query < Norikra::Client::Query
 
-    # This command try to see all fields. But can not do well with container fields .
-    desc "see TARGET", "see event stream of TARGET."
-    option :format, :type => :string, :default => 'json', :desc => "format of output data per line of stdout [json(default), ltsv]"
-    option :time_key, :type => :string, :default => 'time', :desc => "output key name for event time (default: time)"
-    option :time_format, :type => :string, :default => '%Y/%m/%d %H:%M:%S', :desc => "output time format (default: '2013/05/14 17:57:59')"
-    option :timeout, :type => :numeric, :default => nil, :desc => "Timeout in specified seconds (default: nil = no timeout)", :aliases => :t
-    def see(target)
-      formatter = formatter(options[:format])
-      time_formatter = lambda{|t| Time.at(t).strftime(options[:time_format])}
-      
-      begin
-        res = Net::HTTP.get(options[:host],"/stat/dump",options[:http_port])
-        stats = JSON.parse(res)
-        if (target_info = stats['targets'].select{  |i| i['name'] == target}.first).size == 0 
-          STDERR.puts "No such target"
-          exit 1
-        end
-
-        STDERR.puts target_info.to_s
-        if target_info['fields'].size == 0
-          STDERR.puts "No fields registered"
-          exit 1
-        end
-        
-        query = %(SELECT #{target_info['fields'].map{  |k,v| "nullable(#{k})" }.join(',')} FROM #{target_info['name']})
-        STDERR.puts query
-        query_name = QUERY_NAME_PREFIX + "_select_all_#{target_info['name']}"
-
-        wrap do 
-          client(parent_options).register(query_name,QUERY_GROUP, query)
-        end
-
-        timeout(options[:timeout]) do 
-          while true 
-            wrap do 
-              client(parent_options).event(query_name).each do |time,event|
-                event = {options[:time_key] => Time.at(time).strftime(options[:time_format])}.merge(event)
-                puts formatter.format(event)
-              end
-            end
-            sleep 1
-          end
-        end
-      rescue Timeout::Error, Interrupt # Normal end
-      ensure
-        wrap do 
-          client(parent_options).deregister(query_name)
-        end
-      end
-    end
-  end
-
-  class Query < Thor
-    include Norikra::Client::CLIUtil
-    QUERY_NAME_PREFIX = "norikra-tmp-"
-    QUERY_GROUP = "norikra-helper"
-
-    desc "test QUERY", "test QUERY. Register temporary query"
-    option :format, :type => :string, :default => 'json', :desc => "format of output data per line of stdout [json(default), ltsv]"
-    option :time_key, :type => :string, :default => 'time', :desc => "output key name for event time (default: time)"
-    option :time_format, :type => :string, :default => '%Y/%m/%d %H:%M:%S', :desc => "output time format (default: '2013/05/14 17:57:59')"
+    desc "test QUERY", "test QUERY. Register temporary query and periodically fetch result"
     option :query_file, :type => :string, :default => nil, :desc => "read query from file", :aliases => :f
+    option :format, :type => :string, :default => 'json', :desc => "format of output data per line of stdout [json(default), ltsv]"
+    option :time_key, :type => :string, :default => 'time', :desc => "output key name for event time (default: time)"
+    option :time_format, :type => :string, :default => '%Y/%m/%d %H:%M:%S', :desc => "output time format (default: '2013/05/14 17:57:59')"
     def test(query=nil)
 
       if query && options[:query_file]
@@ -86,21 +25,17 @@ module Norikra::Helper
         query = File.open(options[:query_file]).read
       end
 
-      
-      formatter = formatter(options[:format])
-      time_formatter = lambda{|t| Time.at(t).strftime(options[:time_format])}
-
       query_name = QUERY_NAME_PREFIX + "#{$$}_#{Time.now.to_i}"
       client(parent_options).register(query_name, QUERY_GROUP, query)
       puts "Registered query: #{query_name}"
       begin
         while true
-          client(parent_options).event(query_name).each do |time,event|
-            event = {options[:time_key] => Time.at(time).strftime(options[:time_format])}.merge(event)
-            puts formatter.format(event)
-          end
+          x = Norikra::Client::Event.new([],options)
+          x.parent_options = parent_options
+          x.fetch(query_name)
           sleep 1
         end
+      rescue Interrupt # Normal end
       ensure
         client(parent_options).deregister(query_name)
       end
@@ -111,10 +46,10 @@ module Norikra::Helper
     def replace(name, query=nil)
 
       if query && options[:query_file]
-        STDERR.puts "Both query string and --query_file were specified"
+        puts "Both query string and --query_file were specified"
         exit 1
       elsif not (query || options[:query_file])
-        STDERR.puts "Please specify query string or file."
+        puts "Please specify query string or file."
         exit 1
       elsif options[:query_file]
         query = File.open(options[:query_file]).read
@@ -122,53 +57,59 @@ module Norikra::Helper
 
       current_query = client(parent_options).queries.select{  |q| q['name'] == name}.first
       unless current_query
-        STDERR.puts "Query with name: #{name} does not exist"
+        puts "Query with name: #{name} does not exist"
         exit 1
       end
 
-      # try registering new query for syntax check
+      # try registering new query with temporary name for syntax check
       begin
         tmp_query_name = QUERY_NAME_PREFIX + "#{$$}_#{Time.now.to_i}"
         client(parent_options).register(tmp_query_name, QUERY_GROUP, query)
       rescue => e
-        STDERR.puts "Error in registering query. #{$!}"
+        puts "Error in temporarily registering query. #{$!}"
         exit 1
       ensure
-        if client(parent_options).queries.select{  |q| q['name'] == tmp_query_name}.first
-          client(parent_options).deregister(tmp_query_name)
+        wrap do 
+          if client(parent_options).queries.select{  |q| q['name'] == tmp_query_name}.first
+            client(parent_options).deregister(tmp_query_name)
+          end
         end
       end
 
       # Actually de-regsister and register new query
-      STDERR.puts "Replacing query <#{name}>, old_expression: #{current_query['expression']}, new_expression: #{query}"
-      client(parent_options).deregister(name)
-      client(parent_options).register(name, current_query['group'], query)
+      puts "Replacing query <#{name}>, old_expression: #{current_query['expression']}, new_expression: #{query}"
+      wrap do 
+        client(parent_options).deregister(name)
+        client(parent_options).register(name, current_query['group'], query)
+      end
     end
+
   end
 
-  class Event < Thor
-    include Norikra::Client::CLIUtil
+  class Event < Norikra::Client::CLI
 
     desc "replay TARGET FILE", "Send saved events in FILE to TARGET"
     option :batch_size, :type => :numeric, :default => 10000, :desc => "records sent in once transferring (default: 10000)"
-    option :batch_interval, :type => :numeric, :default => 0, :desc => "Wait time in seconds between each transferring (default: 0)", :aliases => :i
     def replay(target, file)
       client = client(parent_options)
       parser = parser("json")
       buffer = []
+      sleep_sec = 0
       File.open(file).each_line do |line|
         ## if line is '# <number>' format, wait <number> seconds.
         if /^#\s+(\d+)$/ =~ line
-          do_sleep = sleep $1.to_f
+          sleep_sec = $1.to_f
         else
           buffer.push(parser.parse(line))
         end
-        if buffer.size >= options[:batch_size] || do_sleep
+        if buffer.size >= options[:batch_size] || sleep_sec > 0
           client.send(target, buffer)
           buffer = []
-          sleep options[:batch_interval]
         end
-        sleep do_sleep if do_sleep
+        if sleep_sec > 0
+          sleep sleep_sec
+          sleep_sec = 0
+        end
       end
 
       wrap do
@@ -176,16 +117,17 @@ module Norikra::Helper
       end
 
     end
+
   end
   
   class HelperCLI < Norikra::Client::CLI
     class_option :http_port, :type => :numeric, :default => 26578
     
-    desc "target CMD ...ARGS", "use targets"
-    subcommand "target", Norikra::Helper::Target
-    desc "query CMD ...ARGS", "use query"
+    desc "query CMD ...ARGS", "manage queries"
     subcommand "query", Query
-    desc "event CMD ...ARGS", "send events"
+
+    desc "event CMD ...ARGS", "send/fetch events"
     subcommand "event", Event
   end
+  
 end
